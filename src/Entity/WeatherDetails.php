c3cfd940facc0c5be44b6f45431602342412233f
Nath M <nath83@live.co.uk>
2019-10-18 12:21:19 +0100

    Added openweather.org API and styled HTML file

--- src/Entity/WeatherDetails.php
+++ src/Entity/WeatherDetails.php
@@ -2,6 +2,7 @@
 
 namespace App\Entity;
 
+use Symfony\Component\HttpClient\HttpClient;
 /**
 *
 *  Class WeatherDetails 
@@ -21,18 +22,19 @@ class WeatherDetails implements WeatherProvider
 	*
 	* Constructor for WeatherDetails
 	*/
-	public function __construct($providerName, $todaysHigh, $todaysLow, $summary)
+	public function __construct()
 	{
-		$this->providerName = $providerName;
-		$this->todaysHigh = $todaysHigh;
-		$this->todaysLow = $todaysLow;
-		$this->summary = $summary;
+		$this->providerName = "";
+		$this->todaysHigh = "";
+		$this->todaysLow = "";
+		$this->summary = "";
 	}
+
 	/**
 	*  Gets weather forcast providers Name.
 	*  @return String 
 	*/
-	public function getProviderName():string
+	public function getProviderName(): string
 	{
 
 		return $this->providerName;
@@ -42,7 +44,7 @@ class WeatherDetails implements WeatherProvider
 	*  Sets name of today's weather forcast provider.
 	*  @param String $providerName 
 	*/
-	public function setProviderName($providerName)
+	public function setProviderName($providerName): string
 	{
 
 		$this->providerName = $providerName;
@@ -53,7 +55,7 @@ class WeatherDetails implements WeatherProvider
 	*  Gets today's highest temperature. i.e. 9C/50F
 	*  @return String 
 	*/
-	public function getTodaysHigh():string
+	public function getTodaysHigh(): string
 	{
 		return $this->todaysHigh;
 	}
@@ -62,7 +64,7 @@ class WeatherDetails implements WeatherProvider
 	*  Sets today's highest temperature.
 	*  @param String $todaysHigh 
 	*/
-	public function setTodaysHigh($todaysHigh)
+	public function setTodaysHigh($todaysHigh): string
 	{
 		$this->todaysHigh = $todaysHigh;
 		return $this;
@@ -72,7 +74,7 @@ class WeatherDetails implements WeatherProvider
 	*  Gets today's lowest temperature. i.e. 5C/41F
 	*  @return String 
 	*/
-	public function getTodaysLow():string
+	public function getTodaysLow(): string
 	{
 		return $this->todaysLow;
 	}
@@ -81,7 +83,7 @@ class WeatherDetails implements WeatherProvider
 	*  Sets today's lowest temperature.
 	*  @param String $todaysLow 
 	*/
-	public function setTodaysLow($todaysLow)
+	public function setTodaysLow($todaysLow): string
 	{
 		$this->todaysLow = $todaysLow;
 		return $this;
@@ -90,7 +92,7 @@ class WeatherDetails implements WeatherProvider
 	*  Gets today's summary. i.e. Rain and spells
 	*  @return String 
 	*/
-	public function getSummary()
+	public function getSummary(): string
 	{
 		return $this->summary;
 	}
@@ -98,14 +100,14 @@ class WeatherDetails implements WeatherProvider
 	*  Sets today's summary.
 	*  @param String $summary 
 	*/
-	public function setSummary($summary)
+	public function setSummary($summary): string
 	{
 		$this->summary = $summary;
 		return $this;
 	}
 
 	/**
-	* Retrieve the current weather summary for a given location
+	* Retrieve the current weather summary for a given location uses openweathermap.org SOAP api
 	*
 	* @param string $location The name of a UK based location, i.e.
 	'Manchester' or 'London'
@@ -115,7 +117,70 @@ class WeatherDetails implements WeatherProvider
 	 */
 	public function getCurrentWeather(string $location): WeatherDetails
 	{
+		
+    	$this->openWeatherApi($location); 
+
 		return $this;
+	}
+
+	/**
+	* Helper method
+	* Converts kelvins to celcius
+	* @param stirng $kelvins 
+	* @return string
+	*/
+	public function kelvinsToCelcius($kelvins): string
+	{
+		$celcius = $kelvins - 273.15;
+		return round($celcius);
+
+	}
+
+	/**
+	* Helper method
+	* Converts kelvins to fahrenheit
+	* @param stirng $kelvins 
+	* @return string
+	*/
+	public function kelvinsToFahrenheit($kelvins): string
+	{
+		$Fahrenheit = $kelvins * 9/5 - 459.67;
+		return round($Fahrenheit);
+	}
+
+	/**
+	* Calls openwaether.org SAOP api
+	* @param string $location
+	*/
+	private function openWeatherApi($location): WeatherDetails
+	{
+		$httpClient = HttpClient::create();
+
+		$providerName = 'openWeather.org';
+		$location = $location;
+		$api = 'https://api.openweathermap.org/data/2.5/weather?q=' . $location . ',uk&APPID=e043bc102337e2b03c7baf563b822850';
+		
+    	$response = $httpClient->request('GET', $api);
+    	$statusCode = $response->getStatusCode();
+    	$content = $response->getContent();
+    	$content = $response->toArray();
+        $weather = $content['weather'];
+        $weather = $weather[0];
+
+        $summary = $weather['description'];
+
+        $tempDetails = $content['main'];
+        $highestTemp = $tempDetails['temp_max'];
+
+        $lowestTemp = $content['main'];
+        $lowestTemp = $tempDetails['temp_min'];
+
+        $this->providerName = $providerName;
+		$this->todaysHigh = $this->kelvinsToCelcius($highestTemp) . "C" . addslashes("/") . $this->kelvinsToFahrenheit($highestTemp) . "F";
+		$this->todaysLow = $this->kelvinsToCelcius($lowestTemp) . "C" . addslashes("/") . $this->kelvinsToFahrenheit($lowestTemp) . "F";
+		$this->summary = $summary;
+
+        return $this;
 	}
 
 }
